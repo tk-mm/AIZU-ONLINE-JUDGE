f86714996dae4c2b9db483848559aa36304ceacd
uzuyh <miller_sphere@yahoo.co.jp>
2015-04-15 18:37:44 +0900

    create ALDS1

---
+++ ALDS1/InsertionSort.rb
@@ -0,0 +1,14 @@
+n = gets.to_i
+a = gets.chomp.split(" ").map(&:to_i)
+puts a.join(' ')
+
+1.upto(n-1) do |i|
+  v = a[i]
+  j = i - 1
+  while j >= 0 && a[j] > v
+    a[j + 1] = a[j]
+    j -= 1
+  end
+  a[j + 1] = v
+  puts a.join(' ')
+end
